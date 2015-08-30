e769a046f466a0e3308aba5685d79047236d061a
Eli Tucker <eli@tumblr.com>
2015-08-30 13:11:36 -0700

    added pull to refresh

--- Rotten Tomatoes/MoviesViewController.swift
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -16,38 +16,48 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var networkErrorView: UIView!
     
     var movies: [NSDictionary]?
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+
         KVNProgress.show()
+        loadAndDisplay( { KVNProgress.dismiss() } )
+    }
+
+    func loadAndDisplay(endLoadCallback: () -> Void) {
         
         let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
         // if I need to run on the airplane.
         //        let url = NSURL(string: "file:///Users/eli/dev/codepath/rotten-data.json")!
-
+        
         let request = NSURLRequest(URL: url)
         
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-
+            
             if (error != nil) {
                 self.networkErrorView.hidden = false
                 println("Error!")
                 println(error)
             } else {
+                self.networkErrorView.hidden = true
                 let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
                 if let json = json {
                     self.movies = json["movies"] as? [NSDictionary]
                     self.tableView.reloadData()
                 }
             }
-            KVNProgress.dismiss()
+            endLoadCallback()
         }
         
         tableView.dataSource = self
-        tableView.delegate = self        
+        tableView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -82,6 +92,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
+    
+    // ---- refresh stuff
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
+    func onRefresh() {
+//      For testing:
+//        delay(2, closure: {
+//            self.refreshControl.endRefreshing()
+//        })
+        loadAndDisplay( { self.refreshControl.endRefreshing() } )
+    }
+    
+    // ---- end refresh stuff
     
     // MARK: - Navigation
 
